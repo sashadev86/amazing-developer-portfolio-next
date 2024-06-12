aeab892e8324528a0847b27ec9079b9bf4197f28
Alexsandr Valentinov <djvalentinoff@gmail.com>
2024-06-12 15:21:57 +0300

    feat: add sextion approach and experiens

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
+import Experiens from "@/components/Experiens";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -14,6 +16,8 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experiens />
+        <Approach />
       </div>
     </main>
   ) 
